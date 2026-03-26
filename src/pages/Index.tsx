14fb80598c1fd303d3d4d2a5daa5eb30020f1f1d
brunoMyguelDotCom <brunomyguelmarcall@gmail.com>
2026-03-26 02:59:01 -0300

    v1.2.0 - FIX: logo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,7 +12,7 @@ const Index = () => {
       {/* Logo */}
       <div className="w-full flex justify-center mb-12">
         <img
-          src="public/logo.png"
+          src="src/assets/logo.png"
           alt="Odontologia Tanuri"
           className="h-16 md:h-28 object-contain"
         />
