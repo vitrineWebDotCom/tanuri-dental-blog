97e636b642ee112747ef496c1b4d6b5c6b95136c
brunoMyguelDotCom <brunomyguelmarcall@gmail.com>
2026-04-06 12:13:34 -0300

    DEBUG: ping

--- api/ping.js
+++ api/ping.js
@@ -1,18 +1,17 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
-);
-
 export default async function handler(req, res) {
-    return res.status(200).json({
-        url: process.env.SUPABASE_URL,
-        key: process.env.SUPABASE_ANON_KEY ? 'exists' : 'missing',
-        secret: process.env.PING_SECRET ? 'exists' : 'missing',
-    });
+  return res.status(200).json({
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_ANON_KEY ? 'exists' : 'missing',
+    secret: process.env.PING_SECRET ? 'exists' : 'missing',
+  });
 }
 
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabase = createClient(
+//     process.env.SUPABASE_URL,
+//     process.env.SUPABASE_ANON_KEY
+// );
 
 // export default async function handler(req, res) {
 //   if (req.headers['x-ping-secret'] !== process.env.PING_SECRET) {
